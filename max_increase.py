5dcf2a97c86fac11c1e5aadb330c656f363ebe72
Alena Nidilko <alenkan96@gmail.com>
2022-11-20 21:10:48 +0300

    Add max_increase

---
+++ max_increase.py
@@ -0,0 +1,22 @@
+def increase():
+    numbers = input().split(" ")
+    n, k = list(map(lambda x: int(x), numbers))
+    numbers = input().split(" ")
+    numbers = list(map(lambda x: int(x), numbers))
+    c = 0
+    for i in numbers:
+        return
+
+    print()
+
+
+def rank_digit(n):
+    i = 0
+    while n > 0:
+        n = n//10
+        i += 1
+    return i
+
+
+if __name__ == '__main__':
+    increase()
